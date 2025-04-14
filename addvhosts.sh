1c60398588f2215e1beb1605f4820aabd1ab65e6
adamhaley <adamhaley@gmail.com>
2025-04-14 10:46:03 -0700

    addvhosts script

--- addvhosts.sh
+++ addvhosts.sh
@@ -10,7 +10,7 @@ current_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 hosts_path="/etc/hosts"
 vhosts_path="/etc/apache2/sites-available/"
 vhost_skeleton_path="$current_directory/vhost.skeleton.conf"
-web_root="/var/www/"
+web_root="/var/www/html/"
 
 
 # user input passed as options?
@@ -37,11 +37,28 @@ if [ $relative_doc_root == 0 ]; then
 	read -p "Please enter the site path relative to the web root: $web_root_path" relative_doc_root
 fi
 
+# construct absolute path
+absolute_doc_root=$web_root$relative_doc_root
+
+# create directory if it doesn't exists
+if [ ! -d "$absolute_doc_root" ]; then
+
+	# create directory
+	`mkdir "$absolute_doc_root/"`
+	`chown -R $SUDO_USER:staff "$absolute_doc_root/"`
+
+	# create index file
+	indexfile="$absolute_doc_root/index.html"
+	`touch "$indexfile"`
+	echo "<html><head></head><body>Welcome!</body></html>" >> "$indexfile"
+
+	echo "Created directory $absolute_doc_root/"
+fi
 
 # update vhost
 vhost=`cat "$vhost_skeleton_path"`
 vhost=${vhost//@site_url@/$site_url}
-vhost=${vhost//@site_docroot@/$relative_doc_root}
+vhost=${vhost//@site_docroot@/$absolute_doc_root}
 
 `touch $vhosts_path$site_url.conf`
 echo "$vhost" > "$vhosts_path$site_url.conf"
@@ -61,4 +78,3 @@ echo `/etc/init.d/apache2 restart`
 echo "Process complete, check out the new site at http://$site_url"
 
 exit 0
-
